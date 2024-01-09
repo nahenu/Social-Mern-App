29bbed6093911640463e80af2632bd841b0e27fd
Arpit Goyal <95014916+nahenu@users.noreply.github.com>
2024-01-09 15:41:55 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:6000",
+        target: "https://social-mern-app-three.vercel.app/",
         secure: false,
       },
     },
