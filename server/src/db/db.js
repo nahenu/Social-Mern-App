7c0e7ebaadb514cacaf23fab88d48db167f267a1
Arpit Goyal <95014916+nahenu@users.noreply.github.com>
2024-01-09 16:10:15 +0530

    Update db.js

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb+srv://arpitgoyal841:Js84YDRlAi4M87J1@cluster0.hzyjdpv.mongodb.net/Social?retryWrites=true&w=majority");
+    await mongoose.connect("mongodb+srv://arpitgoyal841:arpitsocial@cluster0.hzyjdpv.mongodb.net/Social?retryWrites=true&w=majority");
     console.log("Database is Connected");
   } catch (error) {
     throw new Error(error);
