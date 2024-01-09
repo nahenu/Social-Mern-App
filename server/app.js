50a54a4900badf1a23f46ef9b1a9f5042ff4ce54
Arpit Goyal <95014916+nahenu@users.noreply.github.com>
2024-01-09 15:25:54 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -6,7 +6,12 @@ import { error } from "./src/errors/error.js";
 const app = express();
 dotenv.config();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin:["https://deploy-mern-1whq.vercel.app"],
+  methods:["POST","GET"],
+  credentials:true
+  
+} ));
 
 app.use("/api/v1", appRouter);
 app.use(error());
