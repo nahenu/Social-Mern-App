e999527ec0e718bd98a44fd33dd28b29394888da
Arpit Goyal <95014916+nahenu@users.noreply.github.com>
2024-01-09 16:21:19 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ const app = express();
 dotenv.config();
 app.use(express.json());
 app.use(cors({
-  origin:["https://social-frontend-liart.vercel.app"],
+  origin:["https://social-mern-frontend.vercel.app"],
   methods:["POST","GET"],
   credentials:true
   
