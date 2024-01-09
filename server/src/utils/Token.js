d64eb1b761e863d4cdbfa11e5c4467ef62258f2b
Arpit Goyal <95014916+nahenu@users.noreply.github.com>
2024-01-09 15:31:31 +0530

    Update Token.js

--- server/src/utils/Token.js
+++ server/src/utils/Token.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const createToken = (id, email, expiresIn) => {
   const payload = { id, email };
-  const token = jwt.sign(payload, process.env.JWT_SECRET, {
+  const token = jwt.sign(payload, "nbhjnbjhbiuyhbnyg8oih", {
     expiresIn,
   });
   return token;
