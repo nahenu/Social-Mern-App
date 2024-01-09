8758eb617cec0c1bfd2e5f0316dcc311097f876a
Arpit Goyal <95014916+nahenu@users.noreply.github.com>
2024-01-09 15:42:36 +0530

    Update connection.js

--- client/src/utils/connection.js
+++ client/src/utils/connection.js
@@ -1,5 +1,5 @@
-export const userSignup = "/api/v1/user/signup";
-export const userLogin = "/api/v1/user/login";
-export const userLogout = "/api/v1/user/logout";
-export const userGoogle = "/api/v1/user/googleuser";
-export const userphoto = "/api/v1/user/uploadimage";
+export const userSignup = "api/v1/user/signup";
+export const userLogin = "api/v1/user/login";
+export const userLogout = "api/v1/user/logout";
+export const userGoogle = "api/v1/user/googleuser";
+export const userphoto = "api/v1/user/uploadimage";
