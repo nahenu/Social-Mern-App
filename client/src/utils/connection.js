3c255db9e826d3c68eba9de930a2503398900e17
Arpit Goyal <95014916+nahenu@users.noreply.github.com>
2024-01-09 16:17:50 +0530

    Update connection.js

--- client/src/utils/connection.js
+++ client/src/utils/connection.js
@@ -1,5 +1,5 @@
-export const userSignup = "api/v1/user/signup";
-export const userLogin = "api/v1/user/login";
-export const userLogout = "api/v1/user/logout";
-export const userGoogle = "api/v1/user/googleuser";
-export const userphoto = "api/v1/user/uploadimage";
+export const userSignup = "https://social-mern-backend-phi.vercel.app/api/v1/user/signup";
+export const userLogin = "https://social-mern-backend-phi.vercel.app/api/v1/user/login";
+export const userLogout = "https://social-mern-backend-phi.vercel.app/api/v1/user/logout";
+export const userGoogle = "https://social-mern-backend-phi.vercel.app/api/v1/user/googleuser";
+export const userphoto = "https://social-mern-backend-phi.vercel.app/api/v1/user/uploadimage";
